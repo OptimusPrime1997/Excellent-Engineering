9eb5c92aa839cc7c4a53aa000ce3f651584e474d
dcf14 <dcf14@software.nju.edu.cn>
2016-07-13 11:29:44 +0800

    fix : 写漏了

--- Modle/src/main/java/impl/oracle_result/SingleComponentResult.java
+++ Modle/src/main/java/impl/oracle_result/SingleComponentResult.java
@@ -7,21 +7,24 @@ public class SingleComponentResult implements Result{
 
     private int index;
     private ResultType resultType;
+    private String componentType;
     private String expect;
 
     public String toXML() {
         String XML = "<singleComponent>\n"
                    + "<index>" + index + "</index>\n"
-                   + "<resultType>" + resultType + "</resultType>\n"
+                   + "<resultType>" + resultType.name() + "</resultType>\n"
+                   + "<componentType>" + componentType + "</componentType>\n"
                    + "<expect>" + expect + "</expect>\n"
                    + "</singleComponent>\n";
 
         return XML;
     }
 
-    public SingleComponentResult(ResultType resultType, int index, String expect) {
-        this.resultType = resultType;
+    public SingleComponentResult(int index, ResultType resultType, String componentType, String expect) {
         this.index = index;
+        this.resultType = resultType;
+        this.componentType = componentType;
         this.expect = expect;
     }
 }
