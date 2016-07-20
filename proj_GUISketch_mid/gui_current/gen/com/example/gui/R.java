aeef0fcbb8878d9737c6e24a3c3bfefbf71b9271
ljh14 <874855266@qq.com>
2016-07-20 20:11:40 +0800

    add some code

--- proj_GUISketch_mid/gui_current/gen/com/example/gui/R.java
+++ proj_GUISketch_mid/gui_current/gen/com/example/gui/R.java
@@ -48,12 +48,13 @@ public final class R {
     public static final class id {
         public static final int RelativeLayout_Item=0x7f070008;
         public static final int btExit=0x7f070002;
-        public static final int gridview=0x7f07000b;
+        public static final int gridview=0x7f07000c;
         public static final int gvFileChooser=0x7f070003;
         public static final int imageView1=0x7f070005;
         public static final int imgBackFolder=0x7f070000;
         public static final int imgFileIcon=0x7f070006;
         public static final int iv_icon=0x7f070009;
+        public static final int list_view=0x7f07000b;
         public static final int tvEmptyHint=0x7f070004;
         public static final int tvFileName=0x7f070007;
         public static final int tvPath=0x7f070001;
@@ -65,13 +66,15 @@ public final class R {
         public static final int activity_pre_main=0x7f030002;
         public static final int filechooser_gridview_item=0x7f030003;
         public static final int item=0x7f030004;
-        public static final int view=0x7f030005;
+        public static final int layout_list=0x7f030005;
+        public static final int view=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int empty_hint=0x7f050002;
         public static final int hello_world=0x7f050005;
         public static final int open_file_error_format=0x7f050003;
+        public static final int sub_menu_test=0x7f050006;
         public static final int title_activity_image_choose=0x7f050001;
         public static final int title_activity_pre_main=0x7f050004;
     }
