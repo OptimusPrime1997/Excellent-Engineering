7607d17a13bf1fd740a605b4c29d18c867072dea
Sam <rww14@software.nju.edu.cn>
2016-07-26 17:45:37 +0800

    develop

---
+++ Modle/ONE_PIECE/AnyMemo/Script/Test01.java
@@ -0,0 +1,53 @@
+package java.lang.test;
+
+import java.lang.demo;
+import com.robotium.solo.Solo;
+import java.lang.demo;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import java.io.*;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class Test01 extends ActivityInstrumentationTestCase2<demo>
+{
+private Solo solo;
+
+@SuppressLint("NewApi")
+public Test0(){
+super(demo.class);
+}
+
+@Override
+public void setUp() throws Exception { 
+ solo = new Solo(getInstrumentation(), getActivity()); 
+ }
+
+@Override
+public void tearDown() throws Exception { 
+ solo.finishOpenedActivities(); 
+ }
+
+/*------ Test Core Function ------*/
+public void testOnClick()
+{
+// Click-TestAction-In-TestState
+solo.clickOnText("总计:28 新卡片:27 复习:1");
+
+// Click-TestAction-In-TestState
+solo.clickOnText("?
+					显示答案
+				");
+
+solo.sleep(1000);
+
+// Assert-Text
+boolean test1 = solo.searchText("1.0");
+
+boolean test_result;
+test_result = test1;
+assertTrue("Test: Failed.", test_result);
+}
+/*--------------------------------*/
+}
